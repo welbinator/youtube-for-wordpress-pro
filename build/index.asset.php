59d5a8b4960c841c326b9080c54b143b47ec3e07
James Welbes <james.welbes@gmail.com>
2026-04-24 22:41:06 -0500

    feat: restore channel select dropdown in blocks, fix channel fallback to new channels system

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks'), 'version' => '557217782f13c8e22765');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-hooks'), 'version' => 'c8698fd27a8709ed7f7d');
---
+++ build/simple-youtube-feed/simple-youtube-feed/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '5d209f89ac270652930c');
